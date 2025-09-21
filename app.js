fbed2b895489b85a971b420baf18715b00006950
cindyc-hz <cindy.mch01@proton.me>
2025-09-20 18:54:41 -0600

    implementa funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 let listaAmigos = []
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+
+    console.log(listaAmigos);
+    if (valorDeUsuario === "") {
+        // Si el campo está vacio, mostrar alerta
+        alert ('Por favor, inserte un nombre');
+    } else {
+        // Si no está vacío, agregar el nombre a la lista
+        listaAmigos.push(valorDeUsuario);
+    }
+    limpiarCaja();
+}
+
+// Vaciar el campo de texto
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
