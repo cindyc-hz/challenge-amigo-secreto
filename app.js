12831a4686e916deba9245a2dd9e6ed2f7847729
cindyc-hz <cindy.mch01@proton.me>
2025-09-20 19:19:03 -0600

    agrega funcion mostrar lista amigos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let listaAmigos = []
 
 function agregarAmigo() {
     let input = document.getElementById('amigo');
+    // Usuario no puede agregar solo espacios
+    let valorDeUsuario = input.value.trim();
 
     console.log(listaAmigos);
     if (valorDeUsuario === "") {
@@ -10,8 +12,25 @@ function agregarAmigo() {
     } else {
         // Si no está vacío, agregar el nombre a la lista
         listaAmigos.push(valorDeUsuario);
+        mostrarAmigos();
     }
     limpiarCaja();
+}
+
+function mostrarAmigos() {
+
+    // Llamar a la lista en HTML
+    let listaEnHTML = document.getElementById('listaAmigos')
+    // limpiar la lista
+    listaEnHTML.innerHTML = "";
+    // Recorrer el array en bucle
+    for (let i = 0; i < listaAmigos.length; i++) {
+        // Agregar los elementos a la lista
+        const amigo = listaAmigos[i];
+        const nuevoElementoLista = document.createElement('li');
+        nuevoElementoLista.textContent = amigo;
+        listaEnHTML.appendChild(nuevoElementoLista);
+    }
 }
 
 // Vaciar el campo de texto
